a60a6deb1c77a37c0936b36ca1c404e6bcab0b2a
Elie KADOCHE <eliekadoche78@gmail.com>
2020-12-30 18:31:17 +0100

    Created game configuration

---
+++ gym_missile_command/config.py
@@ -0,0 +1,76 @@
+"""Environment configuration."""
+
+import numpy as np
+
+# General
+# ------------------------------------------
+
+# Environment height
+HEIGHT = 600
+
+# Environment width
+WIDTH = 1100
+
+# Numpy arrays type
+DTYPE = np.float32
+
+# Reward
+# ------------------------------------------
+
+# Reward for each remaining missile
+REWARD_REMAINING_MISSILE = 5
+
+# Reward for each remaining city
+REWARD_REMAINING_CITY = 10
+
+# Reward for each destroyed city
+REWARD_DESTROYED_CITY = -10
+
+# Cities and anti-missile battery
+# ------------------------------------------
+
+# Radius of a city object
+CITY_RADIUS = 27
+
+# Radius of the anti-missile battery object
+BATTERY_RADIUS = 35
+
+# Enemy missiles
+# ------------------------------------------
+
+# The total number of enemy missiles for 1 episode
+ENEMY_MISSILE_TOTAL = 42
+
+# The probability for an enemy missile to appear at a time step
+ENEMY_MISSILE_PROBA_IN = 0.05
+
+# Radius of an enemy missile object
+ENEMY_MISSILE_RADIUS = 3
+
+# Enemy missile speed
+ENNEMY_MISSILE_SPEED = 7
+
+# Battery missiles
+# ------------------------------------------
+
+# Total number of available battery missiles
+BATTERY_MISSILE_TOTAL = 30
+
+# Radius of an battery missile object
+BATTERY_MISSILE_RADIUS = 7
+
+# Maximum explosion radius
+BATTERY_MISSILE_EXPLOSION_RADIUS = 21
+
+# Battery missile speed
+BATTERY_MISSILE_SPEED = 34
+
+# Colors
+# ------------------------------------------
+
+COLOR_BACKGROUND = "#000000"
+COLOR_CITY = "#0000ff"
+COLOR_ENEMY_MISSILE = "#ff0000"
+COLOR_BATTERY_MISSILE = "#00ff00"
+COLOR_EXPLOSION = "#ffff00"
+COLOR_BATTERY = "#ff0ff0"
