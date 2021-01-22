b6f22de311629465f66126342d59cf8882f22204
Elie KADOCHE <eliekadoche78@gmail.com>
2021-01-22 19:41:16 +0100

    Added packages

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
+"""Setup file."""
+
 from setuptools import setup
 
-setup(name="gym_missile_command",
-      version="1.0",
-      author="Elie KADOCHE",
-      install_requires=["gym", "numpy"],
-      description="Gym environment of the Atari game, Missile Command.",
-      )
+setup(
+    name="gym_missile_command",
+    version="1.0",
+    author="Elie KADOCHE",
+    install_requires=["gym",
+                      "numpy",
+                      "opencv-python",
+                      "pygame"],
+    description="Gym environment of the Atari game, Missile Command.",
+)
