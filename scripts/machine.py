c141cf26b71e03796826dfa17ec03cf7999cc9da
Elie KADOCHE <eliekadoche78@gmail.com>
2021-01-25 15:03:01 +0100

    Added main

--- scripts/machine.py
+++ scripts/machine.py
@@ -2,21 +2,22 @@
 
 import gym
 
-# Create the environment
-env = gym.make("gym_missile_command:missile-command-v0")
+if __name__ == "__main__":
+    # Create the environment
+    env = gym.make("gym_missile_command:missile-command-v0")
 
-# Reset it
-observation = env.reset()
+    # Reset it
+    observation = env.reset()
 
-# While the episode is not finished
-done = False
-while not done:
+    # While the episode is not finished
+    done = False
+    while not done:
 
-    # Select an action (here, a random one)
-    action = env.action_space.sample()
+        # Select an action (here, a random one)
+        action = env.action_space.sample()
 
-    # One step forward
-    observation, reward, done, _ = env.step(action)
+        # One step forward
+        observation, reward, done, _ = env.step(action)
 
-    # Render (or not) the environment
-    env.render()
+        # Render (or not) the environment
+        env.render()
