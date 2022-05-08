3db92e041736b0b21c18b282e5ea7737d6a3d172
Elie KADOCHE <eliekadoche78@gmail.com>
2022-05-08 09:38:33 +0200

    Reformat

--- setup.py
+++ setup.py
@@ -6,9 +6,6 @@ setup(
     name="gym_missile_command",
     version="1.0",
     author="Elie KADOCHE",
-    install_requires=["gym",
-                      "numpy",
-                      "opencv-python",
-                      "pygame"],
+    install_requires=["gym", "numpy", "opencv-python", "pygame"],
     description="Gym environment of the Atari game, Missile Command.",
 )
