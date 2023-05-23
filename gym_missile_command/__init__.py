8a98afdf8c71d40ec9cb342ffd13e976a571ab21
Elie KADOCHE <eliekadoche78@gmail.com>
2023-05-23 15:58:42 +0200

    Better versioning

--- gym_missile_command/__init__.py
+++ gym_missile_command/__init__.py
@@ -1,4 +1,9 @@
+from pathlib import Path
+
 from gymnasium.envs.registration import register
+
+with open(Path(__file__).parent / "version.py") as _version_file:
+    __version__ = _version_file.read().strip()
 
 register(
     id="missile-command-v0",
--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 """Setup file."""
 
+from pathlib import Path
+
 from setuptools import setup
+
+ROOT = Path(__file__).parent
+with open(ROOT / "gym_missile_command" / "version.py") as version_file:
+    VERSION = version_file.read().strip()
 
 setup(
     name="gym_missile_command",
-    version="1.4",
+    version=VERSION,
     author="Elie KADOCHE",
     author_email="eliekadoche78@gmail.com",
-    install_requires=["gymnasium", "numpy", "opencv-python", "pygame"],
+    install_requires=["gymnasium >= 0.28.1",
+                      "numpy >= 1.24.3",
+                      "opencv-python >= 4.7.0.72",
+                      "pygame >= 2.4.0"],
     description="Gym environment of the Atari game, Missile Command.",
     packages=["gym_missile_command"],
     url="https://github.com/ElieKadoche/gym_missile_command.git",
